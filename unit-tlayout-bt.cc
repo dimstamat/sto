1d3bb1c0e13cea792ade5f043ac77a56542dae5f
Dimos <dimstamat@gmail.com>
2017-11-02 01:09:45 -0400

    unit test for the layout binary tree

---
+++ unit-tlayout-bt.cc
@@ -0,0 +1,14 @@
+#undef NDEBUG
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include "Transaction.hh"
+#include "TLayoutBT.hh"
+#include "StringWrapper.hh"
+
+#define GUARDED if (TransactionGuard tguard{})
+
+int main() {
+    return 0;
+}
