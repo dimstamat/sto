065e0dd1f8b9d785fde62f5b6f0ab7fa9f9025bf
Dimos <dimstamat@gmail.com>
2017-11-13 12:32:03 -0500

    added tests for TLayoutBT

--- unit-tlayout-bt.cc
+++ unit-tlayout-bt.cc
@@ -3,20 +3,52 @@
 #include <iostream>
 #include <assert.h>
 #include <vector>
-#include "Transaction.hh"
+#include <thread>
+#include <unistd.h>
+
+//#include "Transaction.hh"
 #include "TLayoutBT.hh"
-#include "StringWrapper.hh"
+//#include "StringWrapper.hh"
 
 #include "rwlock.hh"
 
 
 #define GUARDED if (TransactionGuard tguard{})
 
-int main() {
-	TLayoutBT<int> tree;
-    cout<< tree.size(tree.head) << endl;
-	rwlock lock;
+using namespace std;
 
-	lock.read_lock();	
+rwlock lock;
+
+void thread_f (int i){
+	if (i==9){
+		lock.write_lock();
+		usleep(200000);
+		lock.write_unlock();
+	}
+	else {
+		lock.read_lock();
+		usleep(200000);
+		lock.read_unlock();
+	}
+	
+}
+
+int main() {
+	TLayoutBT<unsigned> tree;
+	dptrtype* dirtyP = tree.llock_.getDirtyP();
+
+	tree.insert(123, dirtyP);
+	cout<< tree.size(tree.head) << endl;
+
+	std::vector<std::thread> threads;
+
+	for (int i=0; i<10; i++){
+		threads.push_back(std::thread(thread_f, i));
+	}
+
+	for (int i=0; i<10; i++){
+		threads[i].join();
+	}
+
 	return 0;
 }
